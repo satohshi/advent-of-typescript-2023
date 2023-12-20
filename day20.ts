d51e33b2c1d3372de3daa0e0eed4b948a75dc84d
Satoshi <no@e.mail>
2023-12-20 15:29:41 +0900

    update

--- day20.ts
+++ day20.ts
@@ -1,17 +1,17 @@
 type ToAsciiArt<
     T extends string,
-    Line extends Array<string> = ['', '', ''],
-    Acc extends Array<string> = []
+    Acc extends Array<string> = [],
+    Line extends Array<string> = ['', '', '']
 > = Uppercase<T> extends `${infer First extends string}${infer Rest}`
     ? First extends keyof Letters
         ? ToAsciiArt<
               Rest,
+              Acc,
               [
                   `${Line[0]}${Letters[First][0]}`,
                   `${Line[1]}${Letters[First][1]}`,
                   `${Line[2]}${Letters[First][2]}`
-              ],
-              Acc
+              ]
           >
-        : ToAsciiArt<Rest, ['', '', ''], [...Acc, ...Line]>
+        : ToAsciiArt<Rest, [...Acc, ...Line]>
     : [...Acc, ...Line]

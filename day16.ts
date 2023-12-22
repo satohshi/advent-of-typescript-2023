c859e9c4f0e0aaaef0a6e6bb0b33c8180bb1d084
Satoshi <no@e.mail>
2023-12-23 02:40:23 +0900

    update

--- day16.ts
+++ day16.ts
@@ -1,11 +1,10 @@
 type ForestSize = 0 | 1 | 2 | 3
 
-type SantaY<T extends Array<string[]>> = keyof {
-    [N in ForestSize as '🎅🏼' extends T[N][number] ? N : never]: unknown
-}
-
-type SantaX<T extends Array<string[]>> = keyof {
-    [N in ForestSize as '🎅🏼' extends T[number][N] ? N : never]: unknown
-}
-
-type FindSanta<T extends Array<string[]>> = [SantaY<T>, SantaX<T>]
+type FindSanta<T extends Array<string[]>> = [
+    keyof {
+        [N in ForestSize as '🎅🏼' extends T[N][number] ? N : never]: unknown
+    },
+    keyof {
+        [N in ForestSize as '🎅🏼' extends T[number][N] ? N : never]: unknown
+    }
+]

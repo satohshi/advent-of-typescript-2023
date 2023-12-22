f0c5e7faec1d95ec3e0f6feb3fcf2d21084f1fcf
Satoshi <no@e.mail>
2023-12-23 02:29:21 +0900

    update

--- day16.ts
+++ day16.ts
@@ -1,26 +1,11 @@
-// Check if an array contains '🎅🏼'.
-type HasSanta<T> = T extends Array<string>
-    ? '🎅🏼' extends T[number]
-        ? true
-        : false
-    : false
+type ForestSize = 0 | 1 | 2 | 3
 
-// Find the index of the array that contains '🎅🏼'
-// and return it as object key
-type SantaYObj<T extends Array<string[]>> = {
-    [N in keyof T as HasSanta<T[N]> extends true ? N : never]: unknown
+type SantaY<T extends Array<string[]>> = keyof {
+    [N in ForestSize as '🎅🏼' extends T[N][number] ? N : never]: unknown
 }
 
-// Find the index of '🎅🏼' and turn it into an object key.
-type SantaXObj<T extends Array<string>> = {
-    [N in keyof T as T[N] extends '🎅🏼' ? N : never]: unknown
+type SantaX<T extends Array<string[]>> = keyof {
+    [N in ForestSize as '🎅🏼' extends T[number][N] ? N : never]: unknown
 }
 
-// Turns object keys into numbers
-type ToNumber<T extends Record<string, unknown>> =
-    keyof T extends `${infer N extends number}` ? N : never
-
-type FindY<T extends Array<string[]>> = ToNumber<SantaYObj<T>>
-type FindX<T extends Array<string[]>> = ToNumber<SantaXObj<T[FindY<T>]>>
-
-type FindSanta<T extends Array<string[]>> = [FindY<T>, FindX<T>]
+type FindSanta<T extends Array<string[]>> = [SantaY<T>, SantaX<T>]

a7bbb403e1ac7952cc6f0111084e0c6768185e74
Satoshi <no@e.mail>
2023-12-23 02:15:50 +0900

    update

--- day12.ts
+++ day12.ts
@@ -1,10 +1,8 @@
-// Find the index of '🎅🏼' and turn it into an object key.
-type IndexObj<T extends Array<string>> = {
-    [N in keyof T as T[N] extends '🎅🏼' ? N : never]: unknown
-}
-
-// Turn the object keys into numbers
-type ToNumber<T extends Record<string, unknown>> =
-    keyof T extends `${infer N extends number}` ? N : never
-
-type FindSanta<T extends Array<string>> = ToNumber<IndexObj<T>>
+type FindSanta<
+    T extends Array<string>,
+    Acc extends Array<unknown> = []
+> = T extends [infer First, ...infer Rest extends Array<string>]
+    ? First extends '🎅🏼'
+        ? Acc['length']
+        : FindSanta<Rest, [...Acc, unknown]>
+    : never

972da37a93065d12e53385c1fc00ada906b76b5e
Satoshi <no@e.mail>
2023-12-24 17:40:14 +0900

    update

--- day21-2.ts
+++ day21-2.ts
@@ -15,64 +15,42 @@ type EmptyBoard = [['  ', '  ', '  '], ['  ', '  ', '  '], ['  ', '  ', '  ']]
 type NewGame = { board: EmptyBoard; state: '❌' }
 type Win = ['❌', '❌', '❌'] | ['⭕', '⭕', '⭕']
 
-// Same as array.with()
-type ArrayWith<
-    Arr extends Array<string>,
-    I extends number,
-    S extends string,
-    Acc extends Array<string> = []
-> = Arr extends [infer F extends string, ...infer Rest extends Array<string>]
-    ? Acc['length'] extends I
-        ? ArrayWith<Rest, I, S, [...Acc, S]>
-        : ArrayWith<Rest, I, S, [...Acc, F]>
-    : Acc
+// Similar to array.with() but returns never if the cell is already taken
+type ArrayWith<Arr extends unknown[], I extends number, S> = {
+    [Key in keyof Arr]: Key extends `${I}`
+        ? Arr[Key] extends TicTacToeEmptyCell
+            ? S
+            : 'Invalid move'
+        : Arr[Key]
+}
 
-// 2D version of ArrayWith. Returns never if the cell is already taken
-type MatrixWith<
-    Arr extends Array<string[]>,
-    Y extends number,
-    X extends number,
-    S extends string,
-    Acc extends Array<string[]> = []
-> = Arr extends [
-    infer F extends Array<string>,
-    ...infer Rest extends Array<string[]>
-]
-    ? Acc['length'] extends Y
-        ? F[X] extends TicTacToeEmptyCell
-            ? MatrixWith<Rest, Y, X, S, [...Acc, ArrayWith<F, X, S>]>
-            : never
-        : MatrixWith<Rest, Y, X, S, [...Acc, F]>
-    : Acc
+// 2D version of ArrayWith.
+type MatrixWith<M extends Array<unknown[]>, I extends [number, number], S> = {
+    [Key in keyof M]: Key extends `${I[0]}` ? ArrayWith<M[Key], I[1], S> : M[Key]
+}
 
 type Move<
     Board extends TicTactToeBoard,
     Position extends TicTacToePositions,
     Chip extends TicTacToeChip
 > = Position extends `${infer Y extends TicTacToeYPositions}-${infer X extends TicTacToeXPositions}`
-    ? MatrixWith<Board, TicTacToeYObj[Y], TicTacToeXObj[X], Chip>
+    ? MatrixWith<Board, [TicTacToeYObj[Y], TicTacToeXObj[X]], Chip>
     : never
 
 type EndState<Board extends TicTactToeBoard> =
-    // Check for horizontal lines
-    | keyof {
-          [Y in 0 | 1 | 2 as Board[Y] extends Win
-              ? `${Board[Y][0]} Won`
-              : never]: unknown
-      }
     // Check for vertical lines
+    | keyof {
+          [Y in 0 | 1 | 2 as Board[Y] extends Win ? `${Board[Y][0]} Won` : never]: unknown
+      }
+    // Check for horizontal lines
     | keyof {
           [X in 0 | 1 | 2 as [Board[0][X], Board[1][X], Board[2][X]] extends Win
               ? `${Board[0][X]} Won`
               : never]: unknown
       }
     // Check for diagonal lines
-    | ([Board[0][0], Board[1][1], Board[2][2]] extends Win
-          ? `${Board[1][1]} Won`
-          : never)
-    | ([Board[0][2], Board[1][1], Board[2][0]] extends Win
-          ? `${Board[1][1]} Won`
-          : never)
+    | ([Board[0][0], Board[1][1], Board[2][2]] extends Win ? `${Board[1][1]} Won` : never)
+    | ([Board[0][2], Board[1][1], Board[2][0]] extends Win ? `${Board[1][1]} Won` : never)
     // Check if the board is filled
     | (TicTacToeEmptyCell extends Board[number][number] ? never : 'Draw')
 
@@ -81,14 +59,11 @@ type Next<T extends TicTactToeBoard, C extends TicTacToeChip> = {
     state: EndState<T> extends never ? Swap<C> : EndState<T>
 }
 
-type TicTacToe<
-    T extends TicTacToeGame,
-    P extends TicTacToePositions
-> = T extends {
+type TicTacToe<T extends TicTacToeGame, P extends TicTacToePositions> = T extends {
     board: infer B extends TicTactToeBoard
     state: infer S extends TicTacToeChip
 }
-    ? Move<B, P, S> extends never
+    ? 'Invalid move' extends Move<B, P, S>[number][number]
         ? T
         : Next<Move<B, P, S>, S>
     : never
--- day23.ts
+++ day23.ts
@@ -12,29 +12,14 @@ type Row = 0 | 1 | 2 | 3 | 4 | 5
 type RowBelow<N extends number> = [1, 2, 3, 4, 5, never][N]
 
 // Same as array.with()
-type ArrayWith<
-    Arr extends Array<string>,
-    I extends number,
-    S extends string,
-    Acc extends Array<string> = []
-> = Arr extends [infer F extends string, ...infer Rest extends Array<string>]
-    ? Acc['length'] extends I
-        ? ArrayWith<Rest, I, S, [...Acc, S]>
-        : ArrayWith<Rest, I, S, [...Acc, F]>
-    : Acc
+type ArrayWith<Arr extends unknown[], I extends number, S> = {
+    [Key in keyof Arr]: Key extends `${I}` ? S : Arr[Key]
+}
 
 // 2D version of ArrayWith
-type MatrixWith<
-    Arr extends Array<string[]>,
-    Y extends number,
-    X extends number,
-    S extends string,
-    Acc extends Array<string[]> = []
-> = Arr extends [infer F extends Array<string>, ...infer Rest extends Array<string[]>]
-    ? Acc['length'] extends Y
-        ? MatrixWith<Rest, Y, X, S, [...Acc, ArrayWith<F, X, S>]>
-        : MatrixWith<Rest, Y, X, S, [...Acc, F]>
-    : Acc
+type MatrixWith<M extends Array<unknown[]>, I extends [number, number], S> = {
+    [Key in keyof M]: Key extends `${I[0]}` ? ArrayWith<M[Key], I[1], S> : M[Key]
+}
 
 // Returns the row number of the last empty cell in the column i.e. the row to place the chip
 type GetRow<T extends Connect4Board, Col extends number> = T[number][Col] extends '  '
@@ -76,7 +61,7 @@ type Rotate45<T extends Connect4Board> = [
 ]
 
 // Technically not needed because test cases don't check for this.
-type Rotate315<T extends Connect4Board> = [
+type RotateMinus45<T extends Connect4Board> = [
     [T[0][3], T[1][4], T[2][5], T[3][6]],
     [T[0][2], T[1][3], T[2][4], T[3][5], T[4][6]],
     [T[0][1], T[1][2], T[2][3], T[3][4], T[4][5], T[5][6]],
@@ -95,7 +80,7 @@ type CheckWin<T extends Connect4Board, C extends Connect4Chips> = true extends C
     : Swap<C>
 
 type GetState<T extends Connect4Board, C extends Connect4Chips> = '  ' extends T[number][number]
-    ? CheckWin<T | Rotate90<T> | Rotate45<T> | Rotate315<T>, C>
+    ? CheckWin<T | Rotate90<T> | Rotate45<T> | RotateMinus45<T>, C>
     : 'Draw'
 
 type Move<Board extends Connect4Board, Col extends number, S extends Connect4Chips> = GetRow<
@@ -103,14 +88,16 @@ type Move<Board extends Connect4Board, Col extends number, S extends Connect4Chi
     Col
 > extends number
     ? {
-          board: MatrixWith<Board, GetRow<Board, Col>, Col, S>
-          state: GetState<MatrixWith<Board, GetRow<Board, Col>, Col, S>, S>
+          board: MatrixWith<Board, [GetRow<Board, Col>, Col], S>
+          state: GetState<MatrixWith<Board, [GetRow<Board, Col>, Col], S>, S>
       }
     : never
 
-type Connect4<Game extends Connect4Game, Col extends number> = Game extends {
+type Connect4<Board extends Connect4Game, Col extends number> = Board extends {
     board: infer Board extends Connect4Board
     state: infer State extends Connect4Chips
 }
     ? Move<Board, Col, State>
     : never
+
+export {}

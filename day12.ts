08aaf5ffa6e474679db5f64d2d7611df1dbce9d9
Satoshi <no@e.mail>
2023-12-27 19:53:42 +0900

    update

--- day12.ts
+++ day12.ts
@@ -1,8 +1,8 @@
-type FindSanta<
-    T extends Array<string>,
-    Acc extends Array<unknown> = []
-> = T extends [infer First, ...infer Rest extends Array<string>]
-    ? First extends '🎅🏼'
-        ? Acc['length']
-        : FindSanta<Rest, [...Acc, unknown]>
+type FindSanta<T extends Array<string>> = T extends [
+    ...infer Rest extends Array<string>,
+    infer Last
+]
+    ? Last extends '🎅🏼'
+        ? Rest['length']
+        : FindSanta<Rest>
     : never
--- day19.ts
+++ day19.ts
@@ -1,13 +1,13 @@
 type Gifts = ['🛹', '🚲', '🛴', '🏄']
 type GiftLooper<T extends number> = [1, 2, 3, 0][T]
-type Decrement<T extends number> = [number, 0, 1, 2, 3, 4][T]
 
 type Rebuild<
     T extends Array<number>,
-    Res extends Array<string> = [],
-    I extends number = 0
+    Acc extends Array<string> = [],
+    I extends number = 0,
+    Curr extends Array<Gifts[number]> = []
 > = T extends [infer Q extends number, ...infer Rest extends number[]]
-    ? Q extends 1
-        ? Rebuild<Rest, [...Res, Gifts[I]], GiftLooper<I>>
-        : Rebuild<[Decrement<Q>, ...Rest], [...Res, Gifts[I]], I>
-    : Res
+    ? Q extends Curr['length']
+        ? Rebuild<Rest, [...Acc, ...Curr], GiftLooper<I>>
+        : Rebuild<T, Acc, I, [...Curr, Gifts[I]]>
+    : Acc
